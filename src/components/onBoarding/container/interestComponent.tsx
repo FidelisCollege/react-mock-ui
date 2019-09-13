edd5e3b55fa8f85c5171f87a2fb3af60a074242b
palak_tal <palak.amralkar@talentica.com>
2019-09-13 14:08:07 +0530

    interest component

--- src/components/onBoarding/container/interestComponent.tsx
+++ src/components/onBoarding/container/interestComponent.tsx
@@ -1,8 +1,38 @@
 import * as React from 'react';
+import {Container, Row, Col} from "reactstrap";
+import OnboardBadgeComponent from "./onboardBadgeComponent";
 
 const InterestComponent = () => {
     return (
-        <p>Profile Component</p>
+        <Container className="h-100 d-flex justify-content-center align-items-center bg-white rounded rounded-md-0">
+            <div className="onboard-wrapper-content">
+                <div className="px-4">
+                    <h1 className="mb-5 onboard-heading">Tell us more about your interests</h1>
+
+                    <div className="search-input-group  w-100">
+                        <input type="text" placeholder="Search for your interests here" className="form-control shadow-none"/>
+                    </div>
+                    <div>
+                        <Row className="mt-3">
+                            <Col md="24 text-left">
+                                <OnboardBadgeComponent/>
+                                <OnboardBadgeComponent/>
+                                <OnboardBadgeComponent/>
+                                <OnboardBadgeComponent/>
+                                <OnboardBadgeComponent/>
+                                <OnboardBadgeComponent/>
+                                <OnboardBadgeComponent/>
+                                <OnboardBadgeComponent/>
+                            </Col>
+                        </Row>
+                    </div>
+
+                    <div className="mt-5">
+                        <button type="button" className="btn btn-primary text-capitalize btn-lg f-16"> continue </button>
+                    </div>
+                </div>
+            </div>
+        </Container>
     );
 }
 
---
+++ src/components/onBoarding/container/onboardBadgeComponent.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+const OnboardBadgeComponent = () => {
+    return (
+
+        <button type="button" className="btn interest-badge mr-2 mb-2 text-primary">football
+            <i className="icon-close ml-3 f-16"></i>
+        </button>
+
+    );
+}
+
+export default  OnboardBadgeComponent;
