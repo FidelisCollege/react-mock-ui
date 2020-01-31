31421bca6413db5cd6fcae78f34bc690fb678622
palak_tal <palak.amralkar@talentica.com>
2020-01-31 14:25:01 +0530

    add experience modal

--- src/common/modal/addExperienceModal.tsx
+++ src/common/modal/addExperienceModal.tsx
@@ -23,42 +23,42 @@ const AddExperienceModal = (props) => {
     return (
         <div>
             <button className="btn btn-outline-secondary text-capitalize f-14 mr-2" onClick={toggle}>add</button>
-            <Modal isOpen={modal} toggle={toggle} className={className}>
+            <Modal isOpen={modal} toggle={toggle} className="h-100 d-flex justify-content-center align-items-center">
                 <ModalHeader toggle={toggle}>Modal title</ModalHeader>
                 <ModalBody>
                     <div>
                         <Row className="modal-row">
-                            <Col md="6" xs="12" className="d-flex flex-column align-items-center modal-column">
+                            <Col md="6" xs="12" className="d-flex flex-column align-items-center modal-column justify-content-center">
                                 <IconProfessionalExperience/>
-                                <a className="">professional experience</a>
+                                <a className="f-12">professional experience</a>
                             </Col>
                             <Col md="6" xs="12" className="d-flex flex-column align-items-center modal-column">
                                 <IconEducationExperience/>
-                                <a className="">education experience</a>
+                                <a className="f-12">education experience</a>
                             </Col>
                             <Col md="6" xs="12" className="d-flex flex-column align-items-center modal-column">
                                 <IconMilitaryExperience/>
-                                <a className="">military experience</a>
+                                <a className="f-12">military experience</a>
                             </Col>
                             <Col md="6" xs="12" className="d-flex flex-column align-items-center modal-column">
                                 <IconOrganization/>
-                                <a href="">professional organization</a>
+                                <a href="" className="f-12">professional organization</a>
                             </Col>
                             <Col md="6" xs="12" className="d-flex flex-column align-items-center modal-column">
                                 <IconScore/>
-                                <a href="">test score</a>
+                                <a href="" className="f-12">test score</a>
                             </Col>
                             <Col md="6" xs="12" className="d-flex flex-column align-items-center modal-column">
                                 <IconAward/>
-                                <a>award/honor/accomplishment</a>
+                                <a className="f-12">award/honor/accomplishment</a>
                             </Col>
                             <Col md="6" xs="12" className="d-flex flex-column align-items-center modal-column">
                                 <IconDegree/>
-                                <a>degree & certificate</a>
+                                <a className="f-12">degree & certificate</a>
                             </Col>
                             <Col md="6" xs="12" className="d-flex flex-column align-items-center modal-column">
                                 <IconSkills/>
-                                <a>knowledge & skills</a>
+                                <a className="f-12">knowledge & skills</a>
                             </Col>
                         </Row>
 
